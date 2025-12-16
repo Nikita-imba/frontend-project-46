080bef66b6ade7c788598513e8b4a7a5fcddae9e
Nikita-imba <jvaleska896@mail.com>
2025-12-16 15:41:55 +0300

    add help and version commands

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const gendiff = (filepath1, filepath2, format = 'stylish') => {
+  // Пока просто заглушка
+  return `Comparing ${filepath1} and ${filepath2} with format: ${format}`;
+};
+
+export default gendiff;
