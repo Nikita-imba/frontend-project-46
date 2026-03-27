494ee5cc6ad854f456a83f2130e057f05e79b538
Nikita-imba <jvaleska896@gmail.com>
2026-03-27 19:34:04 +0300

    style: switch back to 2 spaces and hope for the best

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import buildTree from './buildTree.js'
 import format from './formatters/index.js'
 
 const genDiff = (path1, path2, formatName = 'stylish') => {
-    const data1 = fs.readFileSync(path.resolve(path1), 'utf-8')
-    const data2 = fs.readFileSync(path.resolve(path2), 'utf-8')
+  const data1 = fs.readFileSync(path.resolve(path1), 'utf-8')
+  const data2 = fs.readFileSync(path.resolve(path2), 'utf-8')
 
-    const obj1 = parse(data1, path.extname(path1))
-    const obj2 = parse(data2, path.extname(path2))
+  const obj1 = parse(data1, path.extname(path1))
+  const obj2 = parse(data2, path.extname(path2))
 
-    const diffTree = buildTree(obj1, obj2)
-    return format(diffTree, formatName)
+  const diffTree = buildTree(obj1, obj2)
+  return format(diffTree, formatName)
 }
 
 export default genDiff
