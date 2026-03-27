7e087b396ed94328e1d4b725e3203757805797f5
Nikita-imba <jvaleska896@gmail.com>
2026-03-27 17:33:22 +0300

    feat: complete flat JSON comparison logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
-const genDiff = (filepath1, filepath2) => {
-  // Читаем файлы, превращая пути в абсолютные
-  const fullPath1 = path.resolve(process.cwd(), filepath1);
-  const data1 = fs.readFileSync(fullPath1, 'utf-8');
-  
-  return `Read file 1: ${data1.length} characters`;
+const genDiff = (path1, path2) => {
+  const data1 = JSON.parse(fs.readFileSync(path.resolve(path1), 'utf-8'));
+  const data2 = JSON.parse(fs.readFileSync(path.resolve(path2), 'utf-8'));
+
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+
+  const result = keys.map((key) => {
+    if (!_.has(data1, key)) return `  + ${key}: ${data2[key]}`;
+    if (!_.has(data2, key)) return `  - ${key}: ${data1[key]}`;
+    if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    }
+    return `    ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${result.join('\n')}\n}`;
 };
 
 export default genDiff;
