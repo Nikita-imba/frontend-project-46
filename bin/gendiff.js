1b4db78b2053704e4143b6df6d6952d49030177d
Nikita-imba <jvaleska896@gmail.com>
2026-03-27 17:58:31 +0300

    feat: add plain formatter and format selection option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,21 +1,17 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
-import genDiff from '../src/index.js'; // Импортируем нашу библиотеку
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version', 'output the version number')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
-  .helpOption('-h, --help', 'display help for command')
-  .action((filepath1, filepath2) => {
-    // Вызываем функцию и выводим её результат в консоль
-    const result = genDiff(filepath1, filepath2);
-    console.log(result);
+  .version('1.0.0')
+  .arguments('<path1> <path2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((path1, path2, options) => {
+    console.log(genDiff(path1, path2, options.format));
   });
 
 program.parse(process.argv);
---
+++ src/formatters/index.js
@@ -0,0 +1,1 @@
+import stylish from "./stylish.js";import plain from "./plain.js";const format = (tree, formatName) => {switch (formatName) {case "stylish":return stylish(tree);case "plain":return plain(tree);default:throw new Error(`Unknown format: ${formatName}`);}};export default format;
