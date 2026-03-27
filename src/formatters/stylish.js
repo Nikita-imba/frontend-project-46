8b2ff5446a3bd720b2dde7867434111955a53a38
Nikita-imba <jvaleska896@gmail.com>
2026-03-27 18:14:01 +0300

    fix: eliminate powershell-induced encoding errors

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,3 +1,24 @@
 import _ from 'lodash';
 
-const indent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2); const stringify = (value, depth) => { if (!_.isPlainObject(value)) { return String(value); } const keys = _.keys(value); const lines = keys.map((key) => `${indent(depth + 1)}  ${key}: ${stringify(value[key], depth + 1)}`); return `{\n${lines.join('\n')}\n${indent(depth)}  }`; }; const stylish = (tree) => { const iter = (nodes, depth) => { const lines = nodes.map((node) => { const leftIndent = indent(depth); switch (node.type) { case 'nested': return `${leftIndent}  ${node.key}: {\n${iter(node.children, depth + 1)}\n${leftIndent}  }`; case 'added': return `${leftIndent}+ ${node.key}: ${stringify(node.value, depth)}`; case 'removed': return `${leftIndent}- ${node.key}: ${stringify(node.value, depth)}`; case 'changed': return [`${leftIndent}- ${node.key}: ${stringify(node.oldValue, depth)}`, `${leftIndent}+ ${node.key}: ${stringify(node.newValue, depth)}`].join('\n'); case 'unchanged': return `${leftIndent}  ${node.key}: ${stringify(node.value, depth)}`; default: throw new Error(`Unknown type: ${node.type}`); } }); return lines.join('\n'); }; return `{\n${iter(tree, 1)}\n}`; }; export default stylish;
+const indent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
+const stringify = (value, depth) => {
+  if (!_.isPlainObject(value)) return String(value);
+  const keys = _.keys(value);
+  const lines = keys.map((key) => `${indent(depth + 1)}  ${key}: ${stringify(value[key], depth + 1)}`);
+  return `{\n${lines.join('\n')}\n${indent(depth)}  }`;
+};
+const stylish = (tree) => {
+  const iter = (nodes, depth) => {
+    const lines = nodes.map((node) => {
+      const leftIndent = indent(depth);
+      if (node.type === 'nested') return `${leftIndent}  ${node.key}: {\n${iter(node.children, depth + 1)}\n${leftIndent}  }`;
+      if (node.type === 'added') return `${leftIndent}+ ${node.key}: ${stringify(node.value, depth)}`;
+      if (node.type === 'removed') return `${leftIndent}- ${node.key}: ${stringify(node.value, depth)}`;
+      if (node.type === 'changed') return `${leftIndent}- ${node.key}: ${stringify(node.oldValue, depth)}\n${leftIndent}+ ${node.key}: ${stringify(node.newValue, depth)}`;
+      return `${leftIndent}  ${node.key}: ${stringify(node.value, depth)}`;
+    });
+    return lines.join('\n');
+  };
+  return `{\n${iter(tree, 1)}\n}`;
+};
+export default stylish;
