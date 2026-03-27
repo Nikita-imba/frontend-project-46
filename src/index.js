adee10f7bb0ce692f45b9c8a76dc2cc268c0ca0c
Nikita-imba <jvaleska896@gmail.com>
2026-03-27 17:23:22 +0300

    feat: add file reading logic using fs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-const gendiff = (filepath1, filepath2, format = 'stylish') => {
-  // Пока просто заглушка
-  return `Comparing ${filepath1} and ${filepath2} with format: ${format}`;
+import fs from 'fs';
+import path from 'path';
+
+const genDiff = (filepath1, filepath2) => {
+  // Читаем файлы, превращая пути в абсолютные
+  const fullPath1 = path.resolve(process.cwd(), filepath1);
+  const data1 = fs.readFileSync(fullPath1, 'utf-8');
+  
+  return `Read file 1: ${data1.length} characters`;
 };
 
-export default gendiff;
+export default genDiff;
